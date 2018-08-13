1699dfd1f94fbe7f867dee147a08fcae1bab43a7
davidlawes <davidlawes@hotmail.co.uk>
2018-08-13 14:50:47 +0100

    Updated unit tests for balance and add_money methods. Unit tests passing

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,3 +1,12 @@
 class Oystercard
 
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
+  end
+
+  def add_money
+  end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,4 +1,16 @@
 require "oystercard"
 describe Oystercard do
+  describe "#add_money" do
+    it { is_expected.to respond_to(:add_money) }
+  end
+
+  describe "#balance" do
+    it { is_expected.to respond_to(:balance) }
+
+    it "creates new card with balance 0" do
+      expect(subject.balance).to eq 0
+    end
+
+  end
 
 end
